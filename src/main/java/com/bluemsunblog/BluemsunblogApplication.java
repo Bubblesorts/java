b295d786c28d2ae70520e2ec3c7a360a39115334
zhuxinyue <2130685958@qq.com>
2023-10-11 12:54:54 +0800

    初始代码

---
+++ src/main/java/com/bluemsunblog/BluemsunblogApplication.java
@@ -0,0 +1,13 @@
+package com.bluemsunblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluemsunblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluemsunblogApplication.class, args);
+    }
+
+}
